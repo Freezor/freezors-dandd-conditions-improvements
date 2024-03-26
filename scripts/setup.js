0c569ef90329d9ebf07ff2e081f6a7df8dc9ba7b
Oliver <info@oliver-fries.de>
2024-03-26 21:17:26 +0100

    update content

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,20 +1,85 @@
 Hooks.once("init", function () {
-
-    // Register the condition
-    CONFIG.DND5E.conditionTypes ['bloody'] = 'Blutend';
-    CONFIG.DND5E.conditionTypes ['burning'] = 'Brennend';
-    CONFIG.DND5E.conditionTypes ['calmed'] = 'Beruhigt';
-    CONFIG.DND5E.conditionTypes ['challenged'] = 'Herausgefordert';
-    CONFIG.DND5E.conditionTypes ['chilled'] = 'Gekühlt';
-    CONFIG.DND5E.conditionTypes ['confused'] = 'Verwirrt';
-    CONFIG.DND5E.conditionTypes ['drunk'] = 'Betrunken';
-    CONFIG.DND5E.conditionTypes ['drunken'] = 'Angetrunken';
-    CONFIG.DND5E.conditionTypes ['freezing'] = 'Erfrierend';
-    CONFIG.DND5E.conditionTypes ['frozen'] = 'Gefroren';
-    CONFIG.DND5E.conditionTypes ['hallucinating'] = 'Halluzinierend';
-    CONFIG.DND5E.conditionTypes ['inFlames'] = 'In Flammen';
-    CONFIG.DND5E.conditionTypes ['openWound'] = 'Klaffende Wunde';
-    CONFIG.DND5E.conditionTypes ['passedOut'] = 'Ohnmächtig (Alkohol)';
-    CONFIG.DND5E.conditionTypes ['soakedInOil'] = 'Eingetränkt in Brennöl';
-    CONFIG.DND5E.conditionTypes ['wet'] = 'Nass';
+    registerConditions();
 });
+
+function registerConditions() {
+    const conditionsToAdd = {
+        bloody: {
+            label: "Blutend",
+            icon: "modules/freezors-dandd-conditions-improvements/icons/blood.svg"
+        },
+        burning: {
+            label: "Brennend",
+            icon: "modules/freezors-dandd-conditions-improvements/icons/burning.svg"
+        },
+        calmed: {
+            label: "Beruhigt",
+            icon: "modules/freezors-dandd-conditions-improvements/icons/calmed.svg",
+            reference: "Compendium.dnd5e.rules.JournalEntry.w7eitkpD7QQTB6j0.JournalEntryPage.0b8N4FymGGfbZGpJ"
+        },
+        challenged: {
+            label: "Herausgefordert",
+            icon: "modules/freezors-dandd-conditions-improvements/icons/challenged.svg"
+        },
+        chilled: {
+            label: "Gekühlt",
+            icon: "modules/freezors-dandd-conditions-improvements/icons/chilled.svg"
+        },
+        confused: {
+            label: "Verwirrt",
+            icon: "modules/freezors-dandd-conditions-improvements/icons/confused.svg"
+        },
+        drunk: {
+            label: "Betrunken",
+            icon: "modules/freezors-dandd-conditions-improvements/icons/drunk.svg",
+            statuses: ["drunken"]
+        },
+        drunken: {
+            label: "Angetrunken",
+            icon: "modules/freezors-dandd-conditions-improvements/icons/drunken.svg",
+            reference: "Compendium.freezors-dandd-conditions-improvements.fci-rules.JournalEntry.ARu9VB16lwiq0anD.JournalEntryPage.jaUXjS9nq3AUEYEl"
+        },
+        freezing: {
+            label: "Erfrierend",
+            icon: "modules/freezors-dandd-conditions-improvements/icons/freezing.svg",
+            statuses: ["chilled"]
+        },
+        frozen: {
+            label: "Gefroren",
+            icon: "modules/freezors-dandd-conditions-improvements/icons/frozen.svg",
+            statuses: ["frozen", "chilled"]
+        },
+        hallucinating: {
+            label: "Halluzinierend",
+            icon: "modules/freezors-dandd-conditions-improvements/icons/hallucinating.svg"
+        },
+        inFlames: {
+            label: "In Flammen",
+            icon: "modules/freezors-dandd-conditions-improvements/icons/inFlames.svg",
+            statuses: ["burning"]
+        },
+        openWound: {
+            label: "Klaffende Wunde",
+            icon: "modules/freezors-dandd-conditions-improvements/icons/openWound.svg",
+            statuses: ["bloody"]
+        },
+        passedOut: {
+            label: "Ohnmächtig (Alkohol)",
+            icon: "modules/freezors-dandd-conditions-improvements/icons/passedOut.svg",
+            statuses: ["drunken", "drunk"]
+        },
+        soakedInOil: {
+            label: "Eingetränkt in Brennöl",
+            icon: "modules/freezors-dandd-conditions-improvements/icons/soakedInOil.svg"
+        },
+        wet: {
+            label: "Nass",
+            icon: "modules/freezors-dandd-conditions-improvements/icons/wet.svg",
+            statuses: ["soakedInOil"]
+        }
+    };
+
+    for (const [key, value] of Object.entries(conditionsToAdd)) {
+        CONFIG.DND5E.conditionTypes[key] = value;
+    }
+}
